6b05c2953eced06d7eae2baf805b22c54c7a1d34
skd <sdk@pganjac.com>
2025-04-15 15:33:39 +0530

    addevent tested

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import home from "./pages_js/home"
 import about from "./pages_js/about";
-import {loadmenu} from "./pages_js/menupage"
+import loadmenu from "./pages_js/menupage"
 
 const cont=document.querySelector("#content");
 
