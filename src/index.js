25ef6df5a84d97943008aefebe191eed10e37a66
boopathiraja-06 <boopathiraja060700@gmail.com>
2025-04-15 22:54:58 +0530

    completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 import home from "./pages_js/home"
-import about from "./pages_js/about";
+import about from "./pages_js/about"
 import loadmenu from "./pages_js/menu"
 
 const cont=document.querySelector("#content");
