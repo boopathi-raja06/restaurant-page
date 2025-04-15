7376a3fab3eeb467c219efdcb46e6833c462f70a
skd <sdk@pganjac.com>
2025-04-15 12:57:59 +0530

    started

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-console.log("hello world");
-alert("vanakam")
+import home from "./pages_js/home"
+import about from "./pages_js/about"
+import menu from "./pages_js/menu"
+const cont=document.querySelector("#content");
+Cont.innerHTML=home();
+const homebtn=document.querySelector('#home')
+const menubtn=document.querySelector('#menu')
+const aboutbtn=document.querySelector('#about')
+homebtn.addEventListener("click",home())
